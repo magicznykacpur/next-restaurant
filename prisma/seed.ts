1f397e9abb728eb58426d48a0504d8125176aa91
Kacper Zelichowski <kafejek@gmail.com>
2025-07-13 00:21:57 +0200

    feat: uninstall uuid package, autogenerate id's in seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,32 +1,27 @@
-import { v4 as uuidv4 } from "uuid";
 import { prisma } from "../src/config/prisma.config";
 import { Meal } from "../src/generated/prisma";
 import { Decimal } from "../src/generated/prisma/runtime/library";
 
-const meals: Meal[] = [
+const meals: Omit<Meal, "id">[] = [
   {
-    id: uuidv4(),
     name: "Salmon Nigiri",
     price: Decimal(24.99),
     orderId: null,
     quantity: 0,
   },
   {
-    id: uuidv4(),
     name: "Tuna Roll",
     price: Decimal(21.99),
     orderId: null,
     quantity: 0,
   },
   {
-    id: uuidv4(),
     name: "Eel Avocado",
     price: Decimal(22.99),
     orderId: null,
     quantity: 0,
   },
   {
-    id: uuidv4(),
     name: "California Roll",
     price: Decimal(23.99),
     orderId: null,
